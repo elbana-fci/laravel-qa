f8594355ad0c943dc3ff9eba304bb2012836da4f
elbana.fci <elbana.fci@gmail.com>
2020-06-11 13:26:06 +0200

    build question form

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -5,7 +5,14 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             <div class="card">
-                <div class="card-header">All Questions</div>
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h2>All Questions</h2>
+                        <div class="ml-auto">
+                            <a href="{{ route('questions.create') }}" class="btn btn-outline-secondary">Ask Question</a>
+                        </div>
+                    </div>
+                </div>
 
                 <div class="card-body">
                     @foreach($questions as $question)
@@ -30,7 +37,7 @@
                                     <a href="{{ $question->user->url }}">{{ $question->user->name }}</a>
                                     <small class="text-muted">{{ $question->created_date }}</small>
                                 </p>
-                                {{ $question->body }}
+                                {{ str_limit($question->body, 250) }}
                             </div>
                         </div>
                     <hr>
