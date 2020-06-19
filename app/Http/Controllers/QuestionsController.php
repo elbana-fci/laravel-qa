67c62655c0800f51f04c782d30174bef505dc272
elbana.fci <elbana.fci@gmail.com>
2020-06-19 03:09:20 +0200

    Implement Authorization using Policy

--- app/Http/Controllers/QuestionsController.php
+++ app/Http/Controllers/QuestionsController.php
@@ -8,6 +8,9 @@ use App\Http\Requests\AskQuestionRequest;
 
 class QuestionsController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
     /**
      * Display a listing of the resource.
      *
@@ -65,9 +68,10 @@ class QuestionsController extends Controller
      */
     public function edit(Question $question)
     {
-        if(\Gate::denies('update-question', $question)){
+       /* if(\Gate::denies('update-question', $question)){
             abort(403, 'Access denied');
-        }
+        }*/
+        $this->authorize("update", $question);
         return view('questions.edit', compact('question'));
     }
 
@@ -80,9 +84,11 @@ class QuestionsController extends Controller
      */
     public function update(AskQuestionRequest $request, Question $question)
     {
-        if(\Gate::denies('update-question', $question)){
+        /*if(\Gate::denies('update-question', $question)){
             abort(403, 'Access denied');
-        }
+        }*/
+
+        $this->authorize("update", $question);
 
         $question->update($request->only('title', 'body'));
 
@@ -97,9 +103,11 @@ class QuestionsController extends Controller
      */
     public function destroy(Question $question)
     {
-        if(\Gate::denies('delete-question', $question)){
+        /*if(\Gate::denies('delete-question', $question)){
             abort(403, 'Access denied');
-        }
+        }*/
+
+        $this->authorize("delete", $question);
 
         $question->delete();
 
