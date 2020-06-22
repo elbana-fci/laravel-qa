9d1e7b85476f4ff645800484ac0489da382e9b2f
elbana.fci <elbana.fci@gmail.com>
2020-06-22 02:19:55 +0200

    Implement delete functionality for answer

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
 Route::resource('questions', 'QuestionsController')->except('show');
 Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
 Route::get('/questions/{question}/answers/{answer}/edit', 'AnswersController@edit')->name('answers.edit');
-Route::post('/questions/{question}/answers/{answer}/destroy', 'AnswersController@destroy')->name('answers.destroy');
+Route::delete('/questions/{question}/answers/{answer}/destroy', 'AnswersController@destroy')->name('answers.destroy');
 Route::patch('/questions/{question}/answers/{answer}/update', 'AnswersController@update')->name('answers.update');
 
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
