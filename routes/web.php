ef9b9f1043aa05dee3ed85af6b932605bd8af7ef
elbana.fci <elbana.fci@gmail.com>
2020-06-30 13:17:41 +0200

    Implement many to many relationship and make the favorite button works

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::resource('questions', 'QuestionsController')->except('show');
 Route::post('/questions/{question}/answers', 'AnswersController@store')->name('answers.store');
 Route::get('/questions/{question}/answers/{answer}/edit', 'AnswersController@edit')->name('answers.edit');
@@ -26,6 +30,5 @@ Route::patch('/questions/{question}/answers/{answer}/update', 'AnswersController
 Route::get('/questions/{slug}', 'QuestionsController@show')->name('questions.show');
 Route::post('/answers/{answer}/accept', 'AcceptAnswerController')->name('answers.accept');
 
-Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::post('/questions/{question}/favorites', 'FavoritesController@store')->name('question.favorite');
+Route::delete('/questions/{question}/favorites', 'FavoritesController@destroy')->name('question.unfavorite');
